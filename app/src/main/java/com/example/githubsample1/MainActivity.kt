feabb18363ba36b693b45d0d41a7d8c6934cf71f
taketani91386 <65518028+taketani91386@users.noreply.github.com>
2020-06-22 13:10:35 +0900

    Add files via upload

--- app/src/main/java/com/example/githubsample1/MainActivity.kt
+++ app/src/main/java/com/example/githubsample1/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
+    // コメント追加
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

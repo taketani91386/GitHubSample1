6c02eab51db97e17b7b45d419ac17ade7bb1ff37
taketani <zan12612@nifty.com>
2021-07-30 15:46:45 +0900

    コメントの追加

--- app/src/main/java/com/example/githubsample1/MainActivity.kt
+++ app/src/main/java/com/example/githubsample1/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.githubsample1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// 2021/07/30 コメント編集
+
 class MainActivity : AppCompatActivity() {
     // コメント追加
     override fun onCreate(savedInstanceState: Bundle?) {
